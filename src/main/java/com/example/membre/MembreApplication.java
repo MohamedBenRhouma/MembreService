678af36faa05136955f72ea3d1d7f7f331ecbdc7
Mohamd Ben Rhouma <mohamed.benrhouma@enis.tn>
2023-09-22 16:43:48 +0100

    Revert "Back au precedant"
    This reverts commit d46956588ccae98dd4eedbac9e471a672473996d.

--- src/main/java/com/example/membre/MembreApplication.java
+++ src/main/java/com/example/membre/MembreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MembreApplication {
-//commentaire pour GIT tEST
+
 	public static void main(String[] args) {
 		SpringApplication.run(MembreApplication.class, args);
 	}
